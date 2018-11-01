b87fd5f4bfa7f13ed1668804942e01ea962988fc
Hagrid <scu.imaginarium@gmail.com>
2018-11-01 10:36:55 -0700

    fixes

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour {
+
+
+    // Use this for initialization
+    public float radius = 50.0F;
+    public float power = 100.0F;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+}
+
+void OnTriggerEnter(Collider col)
+{
+
+    Vector3 explosionPos = GameObject.Find("BOD").transform.position;
+
+    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+    {
+        Debug.Log("You pressed the button");
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        }
+    }
+}
+    }
