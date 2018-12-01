b0b9340eeaddc4964eee6fb44e0dfce429d83cac
Bilbo Baggins <scu.imaginarium@gmail.com>
2018-11-30 18:13:03 -0800

    UI fix

--- Assets/Scripts/LoadingScene.cs
+++ Assets/Scripts/LoadingScene.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingScene : MonoBehaviour {
 
     public void ChangeScene()
     {
-        //Debug.Log("Scene Change");
-        //SceneManager.LoadScene("MainScene");
+        Debug.Log("Scene Change");
+        SceneManager.LoadScene("MainScene");
         
     }
 }
